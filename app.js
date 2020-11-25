14e6e7b7b07be8c70740471f8f17829825de215f
Kuasar <anudar666@gmail.com>
2020-11-25 22:42:12 +0800

    second state

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var activePlayer = 1;
 // Тоглогчийн цуглуулсан оноог хадгалдаг хувьсагч
 var score = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалдаг хувьсагч
-var roundScore = 0;
+var roundScore = 0;     
 // Шооны аль талаараа буусныг хадгалдаг хувьсагч (value 1-6 random)
-var dice = Math.floor(Math.random()*6)+1;
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+var diceNumber = Math.floor(Math.random()*6)+1;
+document.getElementById("score-0").textContent = '0';
+document.getElementById("score-1").textContent = '0';
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    var diceNumber = Math.floor(Math.random()*6)+1;
+    diceDom.style.display = "block";
+    diceDom.src = "d-" + diceNumber + ".png";
+});
